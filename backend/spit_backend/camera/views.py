c003d26cb43a8fcc3bfad8c314abe2e694328f36
Param Shendekar <paramvs8@gmail.com>
2021-02-13 12:46:16 +0530

    Camera view

--- backend/spit_backend/camera/views.py
+++ backend/spit_backend/camera/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
