4d2bcdd4ce3f970da738191f0010a8e9f7c71a11
Param Shendekar <paramvs8@gmail.com>
2021-02-13 22:16:12 +0530

    camera urls & view added

--- backend/spit_backend/main_app/urls.py
+++ backend/spit_backend/main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('retailRegister',views.RetailerRegisterView.as_view(),name='register_retailer'),
     path('brandRegister',views.BrandRegisterView.as_view(),name='register_brand'),
+    path('camera/',views.camera_on,name='camera'),
 ]
--- backend/spit_backend/main_app/views.py
+++ backend/spit_backend/main_app/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseServerError, HttpResponseRedirect, StreamingHttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView
+from django.views.decorators import gzip
 from .models import User
 from .forms import RetailerRegisterForm, BrandRegisterForm
+import cv2
+import numpy as np
+import time
+import os
 
 # Create your views here.
 def index(request):
@@ -10,6 +15,33 @@ def index(request):
 
 def login(request):
     return render(request,'login.html')
+
+
+def get_frame():
+    list_imgs = []
+    camera =cv2.VideoCapture(0) 
+    end_time = time.time() + 5
+    while time.time() < end_time:
+        _, img = camera.read()
+        list_imgs.append(img)
+        imgencode=cv2.imencode('.jpg',img)[1]
+        #time.sleep(1)
+        stringData=imgencode.tostring()
+        yield (b'--frame\r\n'b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+    i = 0 
+    path= 'D:\param\Competitions\SPIT 2021\Code\\backend\spit_backend\static\\frames'
+    for img in list_imgs:
+        i += 1
+        cv2.imwrite(os.path.join(path,'cctv-data-'+str(i)+'.jpg'),img)
+    del(camera)
+
+@gzip.gzip_page
+def camera_on(request):
+    try :
+        return StreamingHttpResponse(get_frame(),content_type="multipart/x-mixed-replace;boundary=frame")
+    except :
+        return "error"
+
     
 class RetailerRegisterView(CreateView):
     model = User
