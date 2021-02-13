5675f714a1c4bf511c9d0f5284259cd1f6371015
Vishal Salgond <vnsalgond@gmail.com>
2021-02-13 18:28:09 +0530

    Login Register Working

--- backend/spit_backend/main_app/forms.py
+++ backend/spit_backend/main_app/forms.py
@@ -1,10 +1,14 @@
 from django import forms
+from multiselectfield import MultiSelectField
 from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
+from django.contrib.auth.models import User
 from .models import User, RetailerUser, BrandUser
 
 class RetailerRegisterForm(UserCreationForm):
-
+    mobile_no = forms.CharField(required=True)
+    email_id = forms.EmailField(required=True)
+    address = forms.CharField(required=True)
 
     class Meta(UserCreationForm.Meta):
         model = User
@@ -12,17 +16,25 @@ class RetailerRegisterForm(UserCreationForm):
     @transaction.atomic
     def save(self):
         user = super().save(commit=False)
-        user.is_retailer = True
+        user.is_student = True
         user.save()
-        RetailerUser.objects.create(user=user)
+        retailer = RetailerUser.objects.create(user=user)
+        retailer.mobile_no = self.cleaned_data.get('mobile_no')
+        retailer.email_id = self.cleaned_data.get('email_id')
+        retailer.address = self.cleaned_data.get('address')
+        retailer.no_of_aisles = None
+        retailer.premium_positions = None
+        retailer.top_price = None
+        retailer.bottom_price = None
+        retailer.middle_price = None
+        retailer.middle_price = None
+        # student.interests.add(*self.cleaned_data.get('interests'))
         return user
-
 
 class BrandRegisterForm(UserCreationForm):
     mobile_no = forms.CharField(required=True)
     email_id = forms.EmailField(required=True)
     address = forms.CharField(required=True)
-    brand_logo = forms.ImageField(required=True)
 
     class Meta(UserCreationForm.Meta):
         model = User
@@ -30,7 +42,11 @@ class BrandRegisterForm(UserCreationForm):
     @transaction.atomic
     def save(self):
         user = super().save(commit=False)
-        user.is_retailer = True
+        user.is_student = True
         user.save()
-        BrandUser.objects.create(user=user)
+        brand = BrandUser.objects.create(user=user)
+        brand.mobile_no = self.cleaned_data.get('mobile_no')
+        brand.email_id = self.cleaned_data.get('email_id')
+        brand.address = self.cleaned_data.get('address')
+        # student.interests.add(*self.cleaned_data.get('interests'))
         return user
